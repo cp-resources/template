27a41e14cccd55812378261d73a445c44fcb67a9
Devesh Jha <deveshjha489@gmail.com>
2020-05-30 23:10:03 +0530

    template2

---
+++ template2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+const int mod=1000000007;
+typedef pair<int,int> intpair;
+typedef vector<int> vi;
+typedef vector<string> vs;
+int main(){
+		ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
+ 
